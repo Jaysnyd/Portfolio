5e1d70a0fbe149d84698946e14bfea58e487be9e
Jaden <jaysnyderrr@gmail.com>
2025-10-13 17:38:48 -0400

    Added AWS Certification with link to the pdf certificate. Changed images for network+ and ITF+ certifications

--- jadens-portfolio/src/constants/index.js
+++ jadens-portfolio/src/constants/index.js
@@ -21,16 +21,22 @@ const navLinks = [
 
 const certifications = [
   {
+    name: "AWS Certified Cloud Practitioner",
+    imgPath: "/images/aws-ccp.png",
+    classes: "mb-2 md:mb-2 w-42 md:w-80 lg:w-[300px] object-contain",
+    pdfLink: "/AWS-CCP-Certificate.pdf",
+  },
+  {
     name: "CompTIA IT Fundamentals+",
-    imgPath: "/images/itFundPlus.png",
-    classes: "mb-2 md:mb-10 w-42 md:w-80 lg:w-[300px] object-contain",
-    certLink: "#",
+    imgPath: "/images/itf-cert.png",
+    classes: "mb-2 md:mb-2 w-42 md:w-80 lg:w-[300px] object-contain",
+    pdfLink: "#",
   },
   {
     name: "CompTIA Network+",
-    imgPath: "/images/networkPlus.png",
-    classes: "mb-0 md:mb-13 w-32 md:w-35 lg:w-[225px] object-contain",
-    certLink: "#",
+    imgPath: "/images/netplus-cert.png",
+    classes: "mb-2 md:mb-2 w-42 md:w-80 lg:w-[300px] object-contain",
+    pdfLink: "#",
   },
 ];
 
--- jadens-portfolio/src/sections/Certs.jsx
+++ jadens-portfolio/src/sections/Certs.jsx
@@ -8,22 +8,18 @@ const Certs = () => {
       id="certs"
       className="h-1/2 px-4 pb-16 mb-10 bg-transparent pt-5 flex flex-col items-center justify-center "
     >
-      {/* <h1 className="mb-10 mt-0 pt-0 lg:mt-20 text-1xl sm:text-2xl md:text-4xl font-semibold flex justify-center">
-        CERTIFICATIONS
-      </h1> */}
-
       <BubbleText text="CERTIFICATIONS" />
 
       <div
         id="cert-container"
         className="w-1/2 pb-8 md:pb-2 flex flex-col md:flex-row gap-3 justify-center items-center bg-white border border-black border-2 rounded-lg"
       >
-        {certifications.map(({ name, imgPath, classes, certLink }) => (
+        {certifications.map(({ name, imgPath, classes, pdfLink }) => (
           <div
             key={name}
-            className="w-52 md:w-80 lg:w-[300px] lg:w-1/2 flex justify-center items-center"
+            className="p-3 md:p-3 w-52 md:w-80 lg:w-[300px] lg:w-1/2 flex justify-center items-center"
           >
-            <a href={certLink}>
+            <a href={pdfLink} target="_blank">
               <img src={imgPath} alt={name} className={classes} />
             </a>
           </div>
